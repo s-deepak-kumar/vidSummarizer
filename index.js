c92bce32a0772d2ca46e1dc92feb7e186658e9cf
S Deepak Kumar <dipkfilms@gmail.com>
2022-12-06 01:56:37 +0530

    New Changes

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./config");
 const routes = require("./routes/routes");
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('assets'));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set('/views', __dirname);
+app.set('/views', path.join(__dirname, 'views'));
 
 // Routes
 app.use("/", routes.routes);
